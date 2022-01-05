21268133e10fac650d81f7956e8728e802c7d6a3
Joanna <m.joanna.foss@gmail.com>
2022-01-05 11:13:59 -0600

    in-prog: FizzBuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+//NOT DONE UNTIL YOU CAN:
+//1. Describe line 12.
+//2. Describe for loop.
+//3. Look for 2 other alternative solutions.
+
+public class FizzBuzz {
+    public static void solution(int num1, int num2){
+        int start = num1;
+        int end = num2;
+        List<Integer> range = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+
+        for(int num : range) {
+            if (num % 3 == 0 && num % 5 == 0) {
+                System.out.println("num = " + num + " FIZZBUZZ");
+            } else if (num % 3 == 0) {
+                System.out.println("num = " + num + " FIZZ");
+            } else if (num % 5 == 0) {
+                System.out.println("num = " + num + " BUZZ");
+            } else {
+                System.out.println("num = " + num);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        solution(1, 100);
+    }
+}
