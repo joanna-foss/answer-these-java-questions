6a0329dc716506a52cf6927f4e53820137d00e5a
Joanna <m.joanna.foss@gmail.com>
2022-02-16 12:48:13 -0600

    just no

---
+++ src/MinTravelCost.java
@@ -0,0 +1,9 @@
+public class MinTravelCost {
+//    public static int[][] getMinTravelCost(int[][] roads, int numOfStates, int numOfRoads){
+//
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
