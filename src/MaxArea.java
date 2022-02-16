13858242c53fbbae4af1d6e55c69f4fc94d4efa5
Joanna <m.joanna.foss@gmail.com>
2022-02-16 11:02:24 -0600

    optimal solution

--- src/MaxArea.java
+++ src/MaxArea.java
@@ -1,27 +1,44 @@
 public class MaxArea {
 
     public static int maxArea(int[] heightArray) {
-        int max = 0;
+        int maxArea = 0;
+        int leftPointer = 0;
+        int rightPointer = heightArray.length-1;
 
-        for(int i = 0; i < heightArray.length; i++){
-            for(int j = i + 1; j < heightArray.length; j++) {
-                max = getArea(max, heightArray[i], i,  heightArray[j], j);
+        while(leftPointer < rightPointer) {
+            int currentArea = Math.min(heightArray[leftPointer], heightArray[rightPointer]) * (rightPointer - leftPointer);
+            maxArea = Math.max(maxArea, currentArea);
+            if(heightArray[leftPointer] < heightArray[rightPointer]) {
+                leftPointer++;
+            } else {
+                rightPointer--;
             }
         }
 
-        return max;
+        return maxArea;
     }
+// BRUTE FORCE APPROACH
+//        int max = 0;
+//
+//        for(int i = 0; i < heightArray.length; i++){
+//            for(int j = i + 1; j < heightArray.length; j++) {
+//                max = getArea(max, heightArray[i], i,  heightArray[j], j);
+//            }
+//        }
+//
+//        return max;
+//    }
 
-    public static int getArea(int currentMax, int height1, int index1, int height2, int index2) {
-        int currentArea = index2 - index1;
-        if(height1 > height2) {
-            currentArea *= height2;
-        } else if (height2 >= height1) {
-            currentArea *= height1;
-        }
-        
-        return Math.max(currentMax, currentArea);
-    }
+//    public static int getArea(int currentMax, int height1, int index1, int height2, int index2) {
+//        int currentArea = index2 - index1;
+//        if(height1 > height2) {
+//            currentArea *= height2;
+//        } else if (height2 >= height1) {
+//            currentArea *= height1;
+//        }
+//
+//        return Math.max(currentMax, currentArea);
+//    }
 
     public static void main(String[] args) {
         System.out.println("maxArea(new int[]{4, 3, 2, 1, 4}) = " + maxArea(new int[]{4, 3, 2, 1, 4}));
