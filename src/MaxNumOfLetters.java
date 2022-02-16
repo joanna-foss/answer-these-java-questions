35d31f805981c82c833b310c0786d85317dc8fdb
Joanna <m.joanna.foss@gmail.com>
2022-02-15 23:16:47 -0600

    do not understand the recursion needed in else, ask

---
+++ src/MaxNumOfLetters.java
@@ -0,0 +1,21 @@
+public class MaxNumOfLetters {
+    public static void main(String[] args) {
+
+    }
+
+//    static long getMaxNumOfLetters(int n) {
+//        if(n <= 0) {
+//            return 0;
+//        }
+//
+//        int count = 0;
+//
+//        if (n <= 6) {
+//            count = n;
+//        } else {
+//
+//        }
+//        return count;
+//    }
+
+}
