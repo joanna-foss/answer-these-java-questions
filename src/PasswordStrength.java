8f477c64a40095dcd795b15c8dd1f7d17e9f7444
Joanna <m.joanna.foss@gmail.com>
2022-02-18 22:43:37 -0600

    removed souts

--- src/PasswordStrength.java
+++ src/PasswordStrength.java
@@ -11,16 +11,13 @@ public class PasswordStrength {
         for(int i = 0; i < password.length(); i++) {
             for(int j = i + 1; j <= password.length(); j++) {
                 char[] passwordChars = password.substring(i, j).toCharArray();
-                System.out.println(password.substring(i, j));
                 Set<String> uniqueChars = new HashSet<>();
 
                 for (char element : passwordChars) {
-                    System.out.println("current element: " + element);
                     uniqueChars.add(Character.toString(element));
                 }
 
                 total += uniqueChars.size();
-                System.out.println("total: " + total);
             }
         }
 
