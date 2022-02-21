47daaef8af3aed6ade6859f5f743cc19f2620e59
Joanna <m.joanna.foss@gmail.com>
2022-02-20 21:24:20 -0600

    added targetIndices method, O(n) time complexity

--- src/SumPairs.java
+++ src/SumPairs.java
@@ -35,10 +35,25 @@ public class SumPairs{
         return pairs;
     }
 
+    public static int[] targetIndices(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        return null;
+    }
+
     public static void main(String[] args) {
         int[][] intArr = pairSum(new int[]{1, 2, 3, 4, 5}, 5);
         for(int[] pair : intArr) {
-            System.out.println(Arrays.toString(pair));
+            System.out.println(Arrays.toString(pair)); // [1, 4] [2, 3]
         }
+
+        int[] targetIndices = targetIndices(new int[]{1, 2, 3, 7, 3}, 6);
+        System.out.println(Arrays.toString(targetIndices)); // [2, 4]
     }
 }
